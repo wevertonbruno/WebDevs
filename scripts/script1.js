7ded9c9d8680e6edd5e7d5a5706169e718bd32ec
oieusouweverton <oieusouweverton@hotmail.com>
2019-04-26 02:20:01 -0300

    code editor app

---
+++ scripts/script1.js
@@ -0,0 +1,8 @@
+document.querySelector('.tabs').addEventListener('click', function(ev){
+    let bg = document.querySelector('#text-container');
+    console.log(ev);
+    if(ev.target.classList[0] == 'header-tabs'){
+           console.log(ev);
+    }
+        
+});
